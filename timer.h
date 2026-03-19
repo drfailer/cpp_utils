faba9d351ca459109b61a72059f8fded960dea5d
drfailer <drfailer.42@gmail.com>
2026-03-19 11:43:56 -0400

    simplify timer usage

--- timer.h
+++ timer.h
@@ -2,23 +2,31 @@
 #define CPP_UTILS_TIMER_H
 #include <chrono>
 
-#define DEFAULT_TIMER_PRECISION std::chrono::nanoseconds
-#define timer_start(ID)                                                        \
-    auto _timer_start_##ID = std::chrono::high_resolution_clock::now()
-#define timer_end(ID)                                                          \
-    auto _timer_end_##ID = std::chrono::high_resolution_clock::now()
-#define timer_count(ID)                                                        \
-    std::chrono::duration_cast<DEFAULT_TIMER_PRECISION>(_timer_end_##ID        \
-                                                        - _timer_start_##ID)   \
-        .count()
-#define timer_count_prec(ID, PREC)                                             \
-    std::chrono::duration_cast<std::chrono::PREC>(_timer_end_##ID              \
-                                                  - _timer_start_##ID)         \
-        .count()
+#define timer_start(ID) auto _timer_start_##ID = std::chrono::system_clock::now()
+#define timer_end(ID) auto _timer_end_##ID = std::chrono::system_clock::now()
+#define timer_duration(ID)                                                     \
+    std::chrono::duration_cast<std::chrono::nanoseconds>(_timer_end_##ID  - _timer_start_##ID)
 #define timer_report(ID)                                                       \
-    std::cout << "timer " #ID ": " << timer_count(ID) << std::endl
-#define timer_report_prec(ID, PREC)                                            \
-    std::cout << "timer " #ID ": " << timer_count_prec(ID, PREC) << " " #PREC  \
-              << std::endl
+    std::cout << "timer " #ID ": " << duration_to_string(timer_duration(ID)) << std::endl
+
+inline std::string duration_to_string(std::chrono::nanoseconds const &ns) {
+    std::ostringstream oss;
+
+    // Cast with precision loss
+    auto s = std::chrono::duration_cast<std::chrono::seconds>(ns);
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(ns);
+    auto us = std::chrono::duration_cast<std::chrono::microseconds>(ns);
+
+    if (s > std::chrono::seconds::zero()) {
+        oss << s.count() << "." << std::setfill('0') << std::setw(3) << (ms - s).count() << "s";
+    } else if (ms > std::chrono::milliseconds::zero()) {
+        oss << ms.count() << "." << std::setfill('0') << std::setw(3) << (us - ms).count() << "ms";
+    } else if (us > std::chrono::microseconds::zero()) {
+        oss << us.count() << "." << std::setfill('0') << std::setw(3) << (ns - us).count() << "us";
+    } else {
+        oss << ns.count() << "ns";
+    }
+    return oss.str();
+}
 
 #endif
